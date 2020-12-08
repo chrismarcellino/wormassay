9a8efef0411c8e77e22c70d9e8efc26534219e66
Chris Marcellino <chris@marcellino.org>
2020-12-07 23:23:15 -0600

    Allow multithreaded drawing.

--- Classes/BitmapView.m
+++ Classes/BitmapView.m
@@ -11,16 +11,32 @@
 #import <opencv2/core/core_c.h>
 #import "VideoFrame.h"
 
+@interface BitmapView ()
+
+@property VideoFrame *image;
+
+@end
+
 
 @implementation BitmapView
 
-- (void)drawRect:(NSRect)dirtyRect
+- (instancetype)initWithFrame:(NSRect)frameRect
 {
-    if (_image) {
+    if ((self = [super initWithFrame:frameRect])) {
+        [self setCanDrawConcurrently:YES];
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect      // will be called on an arbitrary thread
+{
+    VideoFrame *image = [self image];
+    
+    if (image) {
         CGContextRef context = (CGContextRef)[[NSGraphicsContext currentContext] graphicsPort];
-        CGImageRef cgImage =  [_image createCGImage];
+        CGImageRef cgImage =  [image createCGImage];
         // Draw the image into the graphics context
-        CGContextDrawImage(context, CGRectMake(0, 0, CGImageGetWidth(cgImage), CGImageGetHeight(cgImage)), cgImage);
+        CGContextDrawImage(context, [self bounds], cgImage);
         CGImageRelease(cgImage);
     } else {
         // Draw black
@@ -31,8 +47,8 @@
 
 - (void)renderImage:(VideoFrame *)image
 {
+    [self setImage:image];
     dispatch_async(dispatch_get_main_queue(), ^{
-        _image = image;
         [self setNeedsDisplay:YES];
     });
 }
